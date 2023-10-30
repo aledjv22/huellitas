ec1aa1d427310f9a62f390a5e12b8a6aadff6680
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-10-29 22:01:19 -0300

    initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
+import Home from './Pages/Home';
+import MyAccount from './Pages/MyAccount';
+import NotFound from './Pages/NotFound';
+import SignIn from './Pages/SignIn';
+import SignUp from './Pages/SignUp';
+
+const AppRoutes = () => {
+  const base = 'huellitas';
+  let routes = useRoutes([
+    {path: base, element: <Home />},
+    {path: base + '/my-account', element: <MyAccount />},
+    {path: base + '/sign-in', element: <SignIn />},
+    {path: base + '/sign-up', element: <SignUp />},
+    {path: base + '*', element: <NotFound />}
+  ]);
+
+  return routes;
+}
+
 function App() {
 
   return (
-    <>
-    <h1 className='text-red-400 text-[40px]'>Huellitas</h1>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
