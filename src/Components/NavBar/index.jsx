5556f4b32b0f2d7dedb260f029c5adbf5c74baa8
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-10-31 22:32:37 -0300

    implementation of context in navbar

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,23 +1,31 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
+import { HuellitasContext } from "../../Context";
 
 function NavBar () {
+  const { isLoggedIn } = React.useContext(HuellitasContext);
+
   const activeStyle = 'hover:underline hoverunderline-offset-4';
 
   return (
     <nav className='flex justify-between items-center fixed z-10 top-0 w-full pt-2 px-8 pb-2 text-sm bg-white'>
-      <NavLink className={activeStyle} to='/huellitas/'>
+      <NavLink to='/huellitas/'
+      className={activeStyle} >
         Huellitas
       </NavLink>
       <ul className='flex items-center gap-3'>
-        <NavLink className={activeStyle} to='/huellitas/sign-in'>
+        <NavLink to='/huellitas/sign-in'
+        className={!isLoggedIn ? activeStyle : 'hidden'} >
           Sign In
         </NavLink>
 
-        <NavLink className={activeStyle} to='/huellitas/sign-up'>
+        <NavLink to='/huellitas/sign-up'
+        className={!isLoggedIn ? activeStyle : 'hidden'} >
           Sign Up
         </NavLink>
 
-        <NavLink className={activeStyle} to='/huellitas/my-account'>
+        <NavLink to='/huellitas/my-account'
+        className={isLoggedIn ? activeStyle : 'hidden'} >
           My Account
         </NavLink>
       </ul>
