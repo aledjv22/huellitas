969a20096b6af409cafde260d551ae6dacbbd2ba
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-11-22 20:59:19 -0300

    animal type filtering

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,6 +5,7 @@ const HuellitasContext = React.createContext();
 function HuellitasProvider ({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [pets, setPets] = useState([]);
+  const [searchByType, setSearchByType] = useState(null);
 
   const API_URL = 'https://db-huellitas-0308351800f8.herokuapp.com/api/v1';
 
@@ -15,14 +16,37 @@ function HuellitasProvider ({ children }) {
       .catch(error => console.log(error));
   },[]);
 
-  
+  // filtering section
+  const [filteredPets, setFilteredPets] = useState(null);
+
+  const filteredByType = (pets, searchByType) => {
+    return pets?.filter(pet => pet.type.toLowerCase() === searchByType.toLowerCase());
+  }
+
+  const filterBy = (searchType, pets, searchByType) => {
+    if (searchType === 'type') {
+      return filteredByType(pets, searchByType);
+    }
+
+    if(!searchType) {
+      return pets;
+    }
+  }
+
+  useEffect(() => {
+    if (searchByType)
+      setFilteredPets(filterBy('type', pets, searchByType));
+
+    if (!searchByType)
+      setFilteredPets(filterBy(null, pets, searchByType));
+  },[pets, searchByType]);
 
   return (
     <HuellitasContext.Provider value={{
       isLoggedIn, 
-      setIsLoggedIn,
       pets,
-      setPets
+      filteredPets,
+      setSearchByType
     }}>
       {children}
     </HuellitasContext.Provider>
