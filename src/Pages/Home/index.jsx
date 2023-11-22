b1d61f435be9bb3f5200b55a18eff345f2315292
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-11-22 00:48:36 -0300

    Implementation of the cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,18 @@
+import React from 'react';
 import Layout from '../../Components/Layout';
+import Card from '../../Components/Card';
+import { HuellitasContext } from '../../Context';
 
 function Home () {
+  const { pets } = React.useContext(HuellitasContext);
+
   return (
     <Layout>
-      <h1>Home</h1>
+      <div className="flex flex-wrap justify-center gap-4">
+        {pets.map((pet) => (
+          <Card key={pet.id} pet={pet} />
+        ))}
+      </div>
     </Layout>
   );
 }
