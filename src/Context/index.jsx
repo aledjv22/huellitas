618e4e3f883bfa7809fa056f42559c0df9481263
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-11-22 00:46:18 -0300

    consumption of the api and storage of the pets

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const HuellitasContext = React.createContext();
 
 function HuellitasProvider ({ children }) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [pets, setPets] = useState([]);
+
+  const API_URL = 'https://db-huellitas-0308351800f8.herokuapp.com/api/v1';
+
+  useEffect(() => {
+    fetch(`${API_URL}/pets`)
+      .then(response => response.json())
+      .then(data => setPets(data))
+      .catch(error => console.log(error));
+  },[]);
+
+  
 
   return (
     <HuellitasContext.Provider value={{
       isLoggedIn, 
-      setIsLoggedIn 
+      setIsLoggedIn,
+      pets,
+      setPets
     }}>
       {children}
     </HuellitasContext.Provider>
