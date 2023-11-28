ee953eeddabbdaca32a223b88aa11d91f3bff4cf
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-11-27 22:08:26 -0300

    improved top margin measurement

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,6 +1,6 @@
 function Layout ({ children }) {
   return (
-    <div className='flex flex-col items-center mt-10'>
+    <div className='flex flex-col items-center mt-11'>
       { children }
     </div>
   );
