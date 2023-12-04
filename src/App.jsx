90dd1b0b5d2cbebf1a2a0ba57e8dad572dfdaf4a
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-12-04 15:36:10 -0300

    My Account is not shown if you are not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { useRoutes, BrowserRouter } from 'react-router-dom';
-import { HuellitasProvider } from './Context';
+import { HuellitasProvider, HuellitasContext } from './Context';
 import Home from './Pages/Home';
 import MyAccount from './Pages/MyAccount';
 import NotFound from './Pages/NotFound';
@@ -8,11 +9,12 @@ import SignUp from './Pages/SignUp';
 import NavBar from './Components/NavBar';
 
 const AppRoutes = () => {
+  const { isLoggedIn } = React.useContext(HuellitasContext);
   const base = 'huellitas';
   let routes = useRoutes([
     {path: base, element: <Home />},
-    {path: base + '/my-account', element: <MyAccount />},
-    {path: base + '/my-account/*', element: <MyAccount />},
+    {path: base + '/my-account', element: isLoggedIn ? <MyAccount /> : <NotFound />},
+    {path: base + '/my-account/*', element: isLoggedIn ? <MyAccount /> : <NotFound />},
     {path: base + '/sign-in', element: <SignIn />},
     {path: base + '/sign-up', element: <SignUp />},
     {path: base + '/*', element: <NotFound />}
