b34f8b71193f0a434b9a924da68090a1d80c6df7
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-02-23 21:51:47 -0300

    Update navbar for mobile version

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -48,23 +48,37 @@ function NavBar () {
 
   const navDesktop = () => {
     return (
-      <nav className="flex justify-between items-center fixed z-10 top-0 w-full h-12 py-1 px-8 text-xl text-[#86155f] font-semibold bg-[#fcb6ee]">
+      <nav className={`flex justify-between items-center fixed z-10 top-0 w-full py-1 px-8 text-[#86155f] bg-[#fcb6ee] 
+      ${isMobile ? "text-sm border border-[#fcb6ee] h-11" : "font-semibold text-xl h-12"}`}>
         <NavLink to="/"
         className={activeStyle}
         >
-          <img src={logo} alt="Huellitas" className="h-12" 
+          <img src={logo} alt="Huellitas" className={`${isMobile ? "h-11" : "h-12"}`} 
           onClick={() => {
             setSearchByType(null);
             setSearchBySex(null);
             setSearchByState(null);
             setSearchBySize(null);
             closeSubmenu();
+            setIsOpen(false);
           }}/>
         </NavLink>
+
+        {isMobile && (
+          <img src={isOpen ? close_icon : menu_icon} alt="Menu" className="h-8 cursor-pointer"
+          onClick={() => {
+            setIsOpen(!isOpen);
+            closeSubmenu();
+          }} />
+        )}
   
-        <ul className="flex items-center gap-3">
+        <ul 
+        className={`${isMobile ? 
+        "flex-col bg-[#fcb6ee] absolute top-11 left-0 w-full font-semibold text-base pb-2" : 
+        "gap-3"} ${isMobile && !isOpen ?  "hidden" : ""} flex items-center`}>
           <NavLink to="/"
-          className={activeStyle}
+          className={`${activeStyle} 
+          ${isMobile ? "flex flex-col items-center w-full py-2 hover:bg-[#fccef4]" : ""}`}
           >
             <div className={`flex ${!isMobile ? "mr-3" : ""}`}>
               <p className={isMobile ? "mr-3" : ""}
@@ -90,11 +104,13 @@ function NavBar () {
                 />
                 {isOpenAll && (
                   <Submenu type="Todo" 
+                    isMobile={isMobile}
                     setSearchBySex={setSearchBySex} 
                     setSearchByType={setSearchByType} 
                     setSearchByState={setSearchByState} 
                     setSearchBySize={setSearchBySize}
                     closeSubmenu={closeSubmenu}
+                    setIsOpenMobileMenu={setIsOpen}
                   />
                 )}
               </div>
@@ -102,7 +118,9 @@ function NavBar () {
           </NavLink>
   
           <NavLink to="/"
-          className={activeStyle}>
+          className={`${activeStyle} 
+          ${isMobile ? "flex flex-col items-center w-full py-2 hover:bg-[#fccef4]" : ""}`}
+          >
             <div className={`flex ${!isMobile ? "mr-3" : ""}`}>
               <p className={isMobile ? "mr-3" : ""}
               onClick={() => {
@@ -127,11 +145,13 @@ function NavBar () {
                 />
                 {isOpenDog && (
                   <Submenu type="Perro" 
+                    isMobile={isMobile}
                     setSearchBySex={setSearchBySex} 
                     setSearchByType={setSearchByType} 
                     setSearchByState={setSearchByState} 
                     setSearchBySize={setSearchBySize}
                     closeSubmenu={closeSubmenu}
+                    setIsOpenMobileMenu={setIsOpen}
                   />
                 )}
               </div>
@@ -139,7 +159,9 @@ function NavBar () {
           </NavLink>
   
           <NavLink to="/"
-          className={activeStyle}>
+          className={`${activeStyle} 
+          ${isMobile ? "flex flex-col items-center w-full py-2 hover:bg-[#fccef4]" : ""}`}
+          >
             <div className={`flex ${!isMobile ? "mr-3" : ""}`}>
               <p className={isMobile ? "mr-3" : ""}
               onClick={() => {
@@ -164,150 +186,83 @@ function NavBar () {
                 />
                 {isOpenCat && (
                   <Submenu type="Gato" 
+                    isMobile={isMobile}
                     setSearchBySex={setSearchBySex} 
                     setSearchByType={setSearchByType} 
                     setSearchByState={setSearchByState} 
                     setSearchBySize={setSearchBySize}
                     closeSubmenu={closeSubmenu}
+                    setIsOpenMobileMenu={setIsOpen}
                   />
                 )}
               </div>
             </div>
           </NavLink>
+
+          {isMobile && (
+            <>
+              <li className={`${!isLoggedIn ? activeStyle : "hidden"} flex w-full justify-evenly py-1`}>
+                <NavLink to="/sign-up"
+                className={`flex flex-col items-center border border-[#86155f] rounded-xl px-3
+                bg-[#fcb6ee] text-[#86155f] hover:bg-[#86155f] hover:text-[#fcb6ee] py-1 `}
+                onClick={() => setIsOpen(false)} >
+                  Registrarse
+                </NavLink>
+
+                <NavLink to="/sign-in"
+                className={`flex flex-col items-center hover:border hover:border-[#86155f] rounded-xl 
+                hover:bg-[#fcb6ee] hover:text-[#86155f] bg-[#86155f] text-[#fcb6ee] py-1 px-3`}
+                onClick={() => setIsOpen(false)} >
+                  Ingresar
+                </NavLink>
+              </li>
+
+              <li 
+              className={`${isLoggedIn ? activeStyle : "hidden"} flex flex-col items-center py-1`}>
+                <NavLink
+                to={`/my-account/${user.firstName}${user.lastName}${user.id.slice(0, 5)}`}
+                className={`flex flex-col items-center hover:border hover:border-[#86155f] rounded-xl 
+                hover:bg-[#fcb6ee] hover:text-[#86155f] bg-[#86155f] text-[#fcb6ee] py-1 px-3`}
+                onClick={() => setIsOpen(false)} >
+                  Mi Cuenta
+                </NavLink>
+              </li>
+            </>
+          )}
         </ul>
   
-        <ul className="flex items-center gap-3 text-[#fcb6ee]">
-          <NavLink to="/sign-up"
-          className={`${!isLoggedIn ? activeStyle : "hidden"} bg-[#a11370] py-1 px-2 rounded-xl`} 
-          onClick={closeSubmenu}
-          >
-            Registrarse
-          </NavLink>
-  
-          <NavLink to="/sign-in"
-          className={`${!isLoggedIn ? activeStyle : "hidden"} bg-[#86155f] py-1 px-2 rounded-xl`} 
-          onClick={closeSubmenu}
-          >
-            Ingresar
-          </NavLink>
-  
-          <NavLink 
-          to={`/my-account/${user.firstName}${user.lastName}${user.id.slice(0, 5)}`}
-          className={`${isLoggedIn ? activeStyle : "hidden"} bg-[#86155f] py-1 px-2 rounded-xl`}  
-          onClick={closeSubmenu}
-          >
-            Mi Cuenta
-          </NavLink>
-        </ul>
+        {!isMobile && (
+          <ul className="flex items-center gap-3 text-[#fcb6ee]">
+            <NavLink to="/sign-up"
+            className={`${!isLoggedIn ? activeStyle : "hidden"} bg-[#a11370] py-1 px-2 rounded-xl`} 
+            onClick={closeSubmenu}
+            >
+              Registrarse
+            </NavLink>
+
+            <NavLink to="/sign-in"
+            className={`${!isLoggedIn ? activeStyle : "hidden"} bg-[#86155f] py-1 px-2 rounded-xl`} 
+            onClick={closeSubmenu}
+            >
+              Ingresar
+            </NavLink>
+
+            <NavLink 
+            to={`/my-account/${user.firstName}${user.lastName}${user.id.slice(0, 5)}`}
+            className={`${isLoggedIn ? activeStyle : "hidden"} bg-[#86155f] py-1 px-2 rounded-xl`}  
+            onClick={closeSubmenu}
+            >
+              Mi Cuenta
+            </NavLink>
+          </ul>
+        )}
       </nav>
     );
   }
-
-  // const navMobile = () => {
-  //   return (
-  //     <nav className="flex justify-between items-center fixed z-10 top-0 w-full h-11 py-1 px-8 text-sm text-[#86155f] bg-[#fcb6ee] border border-[#fcb6ee]">
-  //       <NavLink to="/" className={activeStyle} >
-  //         <img src={logo} alt="Huellitas" className="h-11" 
-  //         onClick={() => {
-  //           setSearchByType(null);
-  //           setSearchBySex(null);
-  //           setSearchByState(null);
-  //           setSearchBySize(null);
-  //           setIsOpenFilter(false);
-  //         }}/>
-  //       </NavLink>
   
-  //       <img src={isOpen ? close_icon : menu_icon} alt="Menu" className="h-8 cursor-pointer"
-  //       onClick={() => setIsOpen(!isOpen)} />
-
-  //       <ul className={`${isOpen ? "flex" : "hidden"} flex-col items-center bg-[#fcb6ee] 
-  //       absolute top-11 left-0 w-full font-semibold text-base pb-2`}>
-  //         <NavLink to="/"
-  //         className={`${activeStyle} flex justify-center w-full py-2 hover:bg-[#fccef4]`}>
-  //           <Submenu type="Todo" 
-  //             setSearchBySex={setSearchBySex} 
-  //             setSearchByType={setSearchByType}
-  //             setSearchByState={setSearchByState} 
-  //             setSearchBySize={setSearchBySize} 
-  //             setIsOpenMobileMenu={setIsOpen}
-  //             isOpenFilter={isOpenFilter}
-  //             setIsOpenFilter={setIsOpenFilter}
-  //           />
-  //         </NavLink>
-
-  //         <NavLink to="/"
-  //         className={`${activeStyle} flex flex-col items-center w-full py-2 hover:bg-[#fccef4]`} >
-  //           <Submenu type="Perro" 
-  //             setSearchBySex={setSearchBySex} 
-  //             setSearchByType={setSearchByType}
-  //             setSearchByState={setSearchByState} 
-  //             setSearchBySize={setSearchBySize} 
-  //             setIsOpenMobileMenu={setIsOpen}
-  //             isOpenFilter={isOpenFilter}
-  //             setIsOpenFilter={setIsOpenFilter}
-  //           />
-  //         </NavLink>
-
-  //         <NavLink to="/"
-  //         className={`${activeStyle} flex flex-col items-center w-full py-2 hover:bg-[#fccef4]`} >
-  //           <Submenu type="Gato" 
-  //             setSearchBySex={setSearchBySex} 
-  //             setSearchByType={setSearchByType}
-  //             setSearchByState={setSearchByState} 
-  //             setSearchBySize={setSearchBySize} 
-  //             setIsOpenMobileMenu={setIsOpen}
-  //             isOpenFilter={isOpenFilter}
-  //             setIsOpenFilter={setIsOpenFilter}
-  //           />
-  //         </NavLink>
-
-  //         <li className={`${!isLoggedIn ? activeStyle : "hidden"} flex w-full justify-evenly py-1`}>
-  //           <NavLink to="/sign-up"
-  //           className={`flex flex-col items-center border border-[#86155f] rounded-xl px-3
-  //           bg-[#fcb6ee] text-[#86155f] hover:bg-[#86155f] hover:text-[#fcb6ee] py-1 `}
-  //           onClick={() => {
-  //             setIsOpen(false);
-  //             setIsOpenFilter(false);
-  //           }}
-  //           >
-  //             Registrarse
-  //           </NavLink>
-
-  //           <NavLink to="/sign-in"
-  //           className={`flex flex-col items-center hover:border hover:border-[#86155f] rounded-xl 
-  //           hover:bg-[#fcb6ee] hover:text-[#86155f] bg-[#86155f] text-[#fcb6ee] py-1 px-3`}
-  //           onClick={() => {
-  //             setIsOpen(false);
-  //             setIsOpenFilter(false);
-  //           }}
-  //           >
-  //             Ingresar
-  //           </NavLink>
-  //         </li>
-
-  //         <li 
-  //         className={`${isLoggedIn ? activeStyle : "hidden"} flex flex-col items-center py-1`}>
-  //           <NavLink
-  //           to={`/my-account/${user.firstName}${user.lastName}${user.id.slice(0, 5)}`}
-  //           className={`flex flex-col items-center hover:border hover:border-[#86155f] rounded-xl 
-  //           hover:bg-[#fcb6ee] hover:text-[#86155f] bg-[#86155f] text-[#fcb6ee] py-1 px-3`}
-  //           onClick={() => {
-  //             setIsOpen(false);
-  //             setIsOpenFilter(false);
-  //           }}
-  //           >
-  //             Mi Cuenta
-  //           </NavLink>
-  //         </li>
-  //       </ul>
-  //     </nav>
-  //   );
-  // }
-
   return (
     <>
-      {!isMobile && navDesktop()}
-      {/* {isMobile && navMobile()} */}
+      {navDesktop()}
     </>
   );
 }
